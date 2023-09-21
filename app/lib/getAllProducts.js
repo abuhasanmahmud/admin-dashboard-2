cafdc591a472df80861e0b13e7282c9642a85be0
abuhasanmahmud <mahmud.me420@gmail.com>
2023-09-21 15:33:14 +0600

    feat(V-1) product page

--- app/lib/getAllProducts.js
+++ app/lib/getAllProducts.js
@@ -1,9 +1,7 @@
 import { baseUrl } from "../page";
 
 export default async function getAllProducts() {
-  const res = await fetch(`${baseUrl}/api/product/products`, {
-    next: { revalidate: 2 },
-  });
+  const res = await fetch(`${baseUrl}/api/product/products`, { cache: "no-store" });
   if (!res.ok) throw new Error("faild to fetch data");
   return res.json();
 }
